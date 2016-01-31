b35c344533de9f135f6aeab0d2104365119ed9cc
Fengda HUANG <h@phodal.com>
2016-01-31 16:38:28 +0800

    add signin logic

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -58,5 +58,39 @@ angular.module('starter.controllers', ['starter.services'])
           }
         });
     };
+  })
+  .controller('SigninCtrl', function ($scope, $http, $state, $ionicPopup) {
+    $scope.user = {
+      username: '',
+      password : '',
+      email: ''
+    };
+
+    $scope.signIn = function() {
+      var payload = {
+        username: $scope.user.username,
+        email: $scope.user.email,
+        password: $scope.user.password
+      };
+
+      $http.post('http://forum.growth.ren/register', payload)
+        .success(function (data) {
+          var alertPopup = $ionicPopup.alert({
+            title: '注册成功',
+            template: '欢迎你，' + $scope.user.username + '。我们已经发送了一封邮件至 ' + $scope.user.email + '，请打开它并完成账号激活。'
+          });
+
+          alertPopup.then(function(res) {
+            $state.go('app.community');
+            console.log('Thank you for not eating my delicious ice cream cone');
+          });
+        })
+        .error(function (data, status) {
+          console.log(data.errors);
+          $scope.errors = data.errors
+        });
+
+    };
+
   });
 
