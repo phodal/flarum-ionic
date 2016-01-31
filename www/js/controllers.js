397143d02f975130f0655f42e744cba08449b1a4
Fengda HUANG <h@phodal.com>
2016-01-31 16:51:32 +0800

    Add for create

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,17 +4,6 @@ angular.module('starter.controllers', ['starter.services'])
 
   })
 
-  .controller('PlaylistsCtrl', function ($scope) {
-    $scope.playlists = [
-      {title: 'Reggae', id: 1},
-      {title: 'Chill', id: 2},
-      {title: 'Dubstep', id: 3},
-      {title: 'Indie', id: 4},
-      {title: 'Rap', id: 5},
-      {title: 'Cowbell', id: 6}
-    ];
-  })
-
   .controller('CommunityCtrl', function ($scope, Discussions, $http, $state, $rootScope) {
     $scope.$on('$ionicView.beforeEnter', function () {
       if ($rootScope.userId) {
@@ -230,5 +219,43 @@ angular.module('starter.controllers', ['starter.services'])
       $scope.userInfo = response.data.attributes;
       $scope.addInfo = response.included;
     });
+  })
+
+  .controller('CreateCtrl', function ($scope, $http, $state, $window) {
+    $scope.post = {
+      title: '',
+      content: ''
+    };
+
+    $scope.isInCreating = false;
+    $scope.create = function (){
+      $scope.isInCreating = true;
+      var data = {
+        "data": {
+          "attributes": {
+            "content": $scope.post.content,
+            "title": $scope.post.title
+          },
+          "relationships": {"tags": {"data": [{"id": "3", "type": "tags"}, {"id": "1", "type": "tags"}]}},
+          "type": "discussions"
+        }
+      };
+      $http({
+        method: 'POST',
+        url: 'http://forum.growth.ren/api/discussions',
+        data: data,
+        headers: {
+          'Authorization': 'Token ' + $window.localStorage.getItem('token')
+        }
+      }).success(function (response) {
+        $scope.isInCreating = false;
+        $state.go('app.topic', {id: response.data.id});
+      }).error(function(data, status){
+        $scope.isInCreating = false;
+        if(status === 401){
+          $scope.modal.show();
+        }
+      })
+    }
   });
 
